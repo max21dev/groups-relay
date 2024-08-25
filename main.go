01d676e10bc4250a2d780c2781e0444ebd49966d
max21dev <max21dev@gmail.com>
2024-08-25 08:58:14 +0200

    disable reaction (kind:7)

--- main.go
+++ main.go
@@ -73,7 +73,7 @@ func main() {
 		policies.PreventLargeTags(64),
 		policies.PreventTooManyIndexableTags(6, []int{9005}, nil),
 		policies.RestrictToSpecifiedKinds(
-			7, 9, 10, 11, 12,
+			9, 10, 11, 12,
 			30023, 31922, 31923, 9802,
 			9000, 9001, 9002, 9003, 9004, 9005, 9006, 9007,
 			9021,
