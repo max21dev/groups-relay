cbc961fd0ca27ea94e44cc425bef68436aad724b
max21dev <max21dev@gmail.com>
2024-11-18 15:46:49 +0100

    Enable user status (kind 1018 and 1068)

--- main.go
+++ main.go
@@ -106,7 +106,8 @@ func main() {
 		policies.PreventLargeTags(64),
 		policies.PreventTooManyIndexableTags(6, []int{9005}, nil),
 		policies.RestrictToSpecifiedKinds(
-			7, 9, 10, 11, 12, 1111,
+			7, 9, 10, 11, 12,
+			1111, 1018, 1068,
 			30023, 31922, 31923, 9802,
 			9000, 9001, 9002, 9003, 9004, 9005, 9006, 9007, 9008,
 			9021, 9022,
