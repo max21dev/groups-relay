ca62efbbc7b0f31aa88c416c652c6955f05cbd7b
max21dev <max21dev@gmail.com>
2024-11-24 15:40:07 +0100

    Enable NIP-56 Reporting (kind 1984)

--- main.go
+++ main.go
@@ -107,7 +107,7 @@ func main() {
 		policies.PreventTooManyIndexableTags(6, []int{9005}, nil),
 		policies.RestrictToSpecifiedKinds(
 			7, 9, 10, 11, 12,
-			1111, 1018, 1068,
+			1111, 1018, 1068, 1984,
 			30023, 31922, 31923, 9802,
 			9000, 9001, 9002, 9003, 9004, 9005, 9006, 9007, 9008,
 			9021, 9022,
